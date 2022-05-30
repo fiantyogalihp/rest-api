0f9887148cb004170a7b8cf092afb8feb034e780
fiantyogalih <fiantiogalih@gmail.com>
2022-05-30 11:20:43 +0700

    fixes conflict version swagger-ui

--- src/main/java/com/kanesa/restapi/RestApiApplication.java
+++ src/main/java/com/kanesa/restapi/RestApiApplication.java
@@ -2,8 +2,10 @@ package com.kanesa.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestApiApplication {
 
 	public static void main(String[] args) {
