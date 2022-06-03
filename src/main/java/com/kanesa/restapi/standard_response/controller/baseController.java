470d5d4c67c5fa39503ad8f2022b2bd5ab57ae87
fiantyogalih <fiantiogalih@gmail.com>
2022-06-03 09:16:11 +0700

    add Throw Exception validation

--- src/main/java/com/kanesa/restapi/standard_response/controller/baseController.java
+++ src/main/java/com/kanesa/restapi/standard_response/controller/baseController.java
@@ -12,13 +12,22 @@ import org.springframework.http.HttpStatus;
 public class baseController {
   // * Throw Exception Handler
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ResponseStatus(code = HttpStatus.BAD_REQUEST)
   @ResponseBody
   public baseResponse<String> handleValidationError(MethodArgumentNotValidException ex) {
     BindingResult result = ex.getBindingResult();
     FieldError error = result.getFieldError();
     String defaultMessage = error.getDefaultMessage();
-    return new baseResponse<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), defaultMessage, false,
+    return new baseResponse<String>(HttpStatus.BAD_REQUEST.value(), defaultMessage, false,
         defaultMessage);
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+  @ResponseBody
+  public baseResponse<String> handleExceptionError(Exception ex) {
+    String message = ex.getMessage();
+    return new baseResponse<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), message, false,
+        message);
   }
 }
